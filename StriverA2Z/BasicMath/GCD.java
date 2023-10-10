faa7a1eb9d1f7423bd2114118d0dbec51810b0d0
ARSHAD <marshadkhn89@gmail.com>
2023-10-10 09:34:04 +0530

    GCD

--- StriverA2Z/BasicMath/GCD.java
+++ StriverA2Z/BasicMath/GCD.java
@@ -2,6 +2,15 @@ package StriverA2Z.BasicMath;
 
 public class GCD {
     public static void main(String[] args) {
-
+        //Drivers Code
+        int num = 4, num2 = 8;
+        int ans = 1;System.out.println(ans);
+        for (int i = 1; i <= Math.min(num, num2); i++) {
+            if (num % i == 0 && num2 % i == 0) {
+                ans = i;
+            }
+        }
+        System.out.println("GCD: " + ans);
     }
 }
+
